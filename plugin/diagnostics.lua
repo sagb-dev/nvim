4048d94ef9fd117800ac632f4ae1ae304f17317f
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2025-08-26 07:17:56 -0300

    feat: updates

--- plugin/diagnostics.lua
+++ plugin/diagnostics.lua
@@ -2,15 +2,15 @@ vim.diagnostic.config({
 	float = {
 		focusable = true,
 		style = "minimal",
-		border = "rounded",
+		-- border = "rounded",
 		source = true, -- Show source in diagnostic popup window
 		header = "",
 		prefix = "",
 	},
-	virtual_lines = {
-		current_line = true,
-	},
-	virtual_text = false,
+	-- virtual_lines = {
+	-- 	current_line = true,
+	-- },
+	-- virtual_text = false,
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = " ",
